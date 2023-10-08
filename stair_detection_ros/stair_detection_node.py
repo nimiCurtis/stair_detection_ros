ccd01cce52613b8a74dd9ca2aee0d741ac8d0ea2
nimiCurtis <nimicu21@gmail.com>
2023-10-08 14:05:00 +0300

    update docs

--- stair_detection_ros/stair_detection_node.py
+++ stair_detection_ros/stair_detection_node.py
@@ -15,10 +15,32 @@ from vision_msgs.msg import Detection2D, Detection2DArray, ObjectHypothesis
 from ultralytics import YOLO
 
 class StairDetectorNode(Node):
-    def __init__(self):
-        super().__init__("stair_detection_node") #Nodes name must be equal to the node
-        self.get_logger().info("NODE is running") #nodes info
+    """StairDetectorNode is a ROS2 node responsible for detecting stair ascending and descending 
+    using the YOLO object detection model.
+    
+    Attributes:
+        cv_bridge: A helper object to convert between ROS Image messages and OpenCV images.
+        image_sub: Subscriber for image topics.
+        detection_data_pub: Publisher for detected object data.
+        detection_img_pub: Publisher for images with annotated detections.
+        model: Loaded YOLO object detection model.
+    
+    Methods:
+        - __init__: Initializes the node and its attributes.
+        - init_params: Loads parameters required by the node.
+        - publish: Publishes the annotated image.
+        - load_model: Loads the YOLO object detection model.
+        - update_detection: Annotates input frame with bounding boxes of detected objects.
+        - predict: Predicts objects in the given frame using the loaded YOLO model.
+        - image_callback: Callback function that processes incoming image data.
+    """
 
+    def __init__(self):
+
+        super().__init__("stair_detection_node") #Nodes name must be equal to the node
+        self.get_logger().info("NODE is running")
+
+        # load parameters
         params = self.init_params()
         
         self.cv_bridge = CvBridge()
@@ -38,7 +60,6 @@ class StairDetectorNode(Node):
         device, model_path = params["device"], params["model_path"]
         self.model = self.load_model(model_path,device)
 
-
     def init_params(self):
         """
         Initialize the parameters for the node from ROS parameters.
@@ -46,7 +67,7 @@ class StairDetectorNode(Node):
         :return: Dictionary of parameters
         :rtype: dict
         """
-        
+
         self.get_logger().info('Parameters: ')
 
         camera_topic = self.declare_parameter('camera_topic', '/zedm/zed_node/rgb/image_rect_color') ## <<< continue
@@ -79,40 +100,75 @@ class StairDetectorNode(Node):
         
         return params_dic
 
-
-    def publish(self, img_msg):
+    def publish(self, img_msg:Image):
         """
-
+        Publishes the image message containing detected objects with bounding boxes.
+        
+        :param img_msg: Image message containing detections
+        :type img_msg: Image
         """
-
+        
         # publish image with bbox
         self.detection_img_pub.publish(img_msg)
 
-
-    def load_model(self,model_path,device):
+    def load_model(self, model_path:String, device:String):
         """
-
+        Loads the YOLO model from the specified path and transfers it to the desired device.
+        
+        :param model_path: Path to the YOLO model file
+        :type model_path: String
+        :param device: Device to load the model on ('cuda' for GPU, 'cpu' for CPU)
+        :type device: String
+        :return: Loaded YOLO model
+        :rtype: YOLO
         """
+        
         # Load the specified model architecture
         model = YOLO(model_path).to(device)
         return model
-    
-    def update_detection(self, results, frame, frame_id):
 
+    def update_detection(self, results, frame):
+        """
+        Updates the input frame with bounding boxes around detected objects.
+        
+        :param results: Detection results from the YOLO model
+        :type results: list
+        :param frame: Input image/frame to be annotated
+        :type frame: ndarray
+        :return: Annotated frame with bounding boxes
+        :rtype: ndarray
+        """
+        # Iterate through the detection results
         for r in results:
             boxes = r.boxes
+            # Process each detected bounding box
             for box in boxes:
-                
+                # Extract bounding box coordinates, confidence score, and class label
                 xyxy = box.xyxy.cpu().detach().numpy().copy()[0]
                 conf = box.conf.cpu().detach().numpy().copy()[0]
                 cls = box.cls.cpu().detach().numpy().copy()[0]
                 cls_name = r.names[cls]
 
-                frame = self.plot_bbox(frame,xyxy,cls_name,conf)
-                
+                # Annotate the frame with the bounding box
+                frame = self.plot_bbox(frame, xyxy, cls_name, conf)
+                    
         return frame
-
+    
     def plot_bbox(self,frame,xyxy,cls_name,conf):
+        """
+        Plot a bounding box of detection on an image
+        
+        :param frame: input frame to be annotated
+        :type results: ndarray
+        :param xyxy: coordinates of the bounding box
+        :type xyxy: ndarray
+        :param cls_name: Label of the detection
+        :type cls_name: string
+        :param conf: Confidence percentage of the detection
+        :type conf: float
+        :return: Annotated frame with bounding boxes
+        :rtype: ndarray
+        """
         font = cv2.FONT_HERSHEY_SIMPLEX
         font_scale = 1
         font_thickness = 2
@@ -136,23 +192,43 @@ class StairDetectorNode(Node):
         cv2.putText(frame, label, (int(xyxy[0]), int(xyxy[1]-5)), font, font_scale, text_color, font_thickness, cv2.LINE_AA)
 
         return frame
-    
-    def predict(self,frame):
-        results = self.model.predict(source=frame,
-                                     verbose = True,
-                                     show=False)
+
+    def predict(self, frame):
+        """
+        Predicts objects in the given frame using the loaded YOLO model.
         
+        :param frame: Image/frame for object detection
+        :type frame: ndarray
+        :return: Detection results from the YOLO model
+        :rtype: list
+        """
+        # Predict objects in the frame using the model
+        results = self.model.predict(source=frame,
+                                    verbose=True,
+                                    show=False)
         return results
-    
+
     def image_callback(self, data:Image):
         """
+        Callback function for image data. Detects objects in the image, annotates it, and publishes the annotated image.
+        
+        :param data: Image message received from the topic
+        :type data: Image
         """
+        
+        # Convert ROS image message to OpenCV format
         cv_img = self.cv_bridge.imgmsg_to_cv2(data, desired_encoding="bgr8")
+        
+        # Predict objects in the image
         result = self.predict(cv_img)
+        
+        # Annotate the image with bounding boxes
         cv_img_with_bbox = self.update_detection(results=result, frame=cv_img)
-
+        
+        # Convert the annotated OpenCV image to ROS image message
         img_msg = self.cv_bridge.cv2_to_imgmsg(cv_img_with_bbox)
         
+        # Publish the annotated image
         self.publish(img_msg)
 
 
@@ -168,7 +244,6 @@ def main():
         running_node.destroy_node()
         rclpy.shutdown()
 
-
     except ROSInterruptException as error:
         print(error)
         running_node.get_logger().error('Caught Exception error, shutting down.\n')
@@ -176,10 +251,7 @@ def main():
         rclpy.shutdown()
         pass
 
-
-
 if __name__ == '__main__':
     main()
-
 
 
